15f569ab9dd9543883ac1b2ad1eba585bc59e614
VIKAS KUMAR PANDIT S <86587232+Vkpandit@users.noreply.github.com>
2025-05-23 14:25:36 +0530

    Fix bug in sales_etl_pipeline.py
    Corrected file path error and added duplicate prevention

--- sales-data-etl/sales_etl_pipeline.py
+++ sales-data-etl/sales_etl_pipeline.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import pyodbc
 
-df = pd.read_csv("sales_data_sample.csv")
+df = pd.read_csv("C:/Users/VIKAS KUMAR PANDIT S/Documents/GitHub/data-engineering-projects/sales-data-etl/sales_data_sample.csv")
 df.dropna(inplace=True)
 df['SaleDate'] = pd.to_datetime(df['SaleDate'])
 
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=YourServer;DATABASE=YourDB;Trusted_Connection=yes;')
+conn = pyodbc.connect('DRIVER={SQL Server};SERVER=PANDITS-LAPTOP\SQLEXPRESS;DATABASE=SQLProject;Trusted_Connection=yes;')
 cursor = conn.cursor()
 
 for index, row in df.iterrows():
-    cursor.execute("""INSERT INTO Sales (Product, Quantity, SaleDate) VALUES (?, ?, ?)""", row.Product, row.Quantity, row.SaleDate)
+    cursor.execute("""
+        IF NOT EXISTS (
+            SELECT 1 FROM Sales WHERE Product = ? AND Quantity = ? AND SaleDate = ?
+        )
+        INSERT INTO Sales (Product, Quantity, SaleDate) VALUES (?, ?, ?)
+    """, row.Product, row.Quantity, row.SaleDate, row.Product, row.Quantity, row.SaleDate)
 
 conn.commit()
